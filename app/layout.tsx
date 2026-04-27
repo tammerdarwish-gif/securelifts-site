0c726b6bc0621ded7469f8136d38e6bbe8c298a8
tammerdarwish-gif <tammerdarwish@gmail.com>
2026-04-27 18:36:58 -0400

    Fix Google Analytics layout setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,11 +5,6 @@ import SiteHeader from "./components/SiteHeader";
 import StickyCTA from "./components/StickyCTA";
 import Script from "next/script";
 
-<Script
-  src="https://www.googletagmanager.com/gtag/js?id=G-NRWSY3V29J"
-  strategy="afterInteractive"
-/>
-
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -20,7 +15,7 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export const metadata = {
+export const metadata: Metadata = {
   metadataBase: new URL("https://securelifts.com"),
   title: "SecureLifts",
   description: "SecureLifts garage door services in South Florida",
@@ -31,23 +26,26 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-return (
-  <html lang="en">
-    <body className={`${geistSans.variable} ${geistMono.variable} antialiased pb-24`}>
-      {/* GTM noscript */}
-      <noscript>
-        <iframe
-          src="https://www.googletagmanager.com/ns.html?id=GTM-NRWSY3V29J"
-          height="0"
-          width="0"
-          style={{ display: "none", visibility: "hidden" }}
-        ></iframe>
-      </noscript>
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased pb-24`}>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-NRWSY3V29J"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-NRWSY3V29J');
+          `}
+        </Script>
 
-      <SiteHeader />
-      {children}
-      <StickyCTA />
-    </body>
-  </html>
-);
+        <SiteHeader />
+        {children}
+        <StickyCTA />
+      </body>
+    </html>
+  );
 }
