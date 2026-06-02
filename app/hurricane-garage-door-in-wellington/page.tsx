13d31cd8e02b15d8240a496781c87610186707a4
tammerdarwish-gif <tammerdarwish@gmail.com>
2026-06-01 22:11:42 -0400

    Add hard redirects for legacy Wellington ad URLs

---
+++ app/hurricane-garage-door-in-wellington/page.tsx
@@ -0,0 +1,5 @@
+import { redirect } from "next/navigation";
+
+export default function LegacyWellingtonHurricaneDoorPage() {
+  redirect("/hurricane-garage-doors/wellington");
+}
---
+++ app/openers-installation-in-wellington/page.tsx
@@ -0,0 +1,5 @@
+import { redirect } from "next/navigation";
+
+export default function LegacyWellingtonOpenerInstallationPage() {
+  redirect("/garage-door-opener-repair/wellington");
+}
