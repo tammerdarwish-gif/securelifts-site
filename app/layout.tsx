dfdc1df6c6935f02784ca362d2a79914b05d640b
tammerdarwish-gif <tammerdarwish@gmail.com>
2026-06-12 08:39:59 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -181,6 +181,19 @@ export default function RootLayout({
 
   return (
     <html lang="en">
+      <head>
+        {/* eslint-disable-next-line @next/next/next-script-for-ga */}
+        <script
+          id="google-tag-manager"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NLKNM4LB');`,
+          }}
+        />
+      </head>
       <body className="antialiased pb-24">
         <BusinessAuthoritySchema />
         <script
